4ecace0edd88a655d40c87d07fd2f06c513fbfc8
Atlant <cuz.den30@gmail.com>
2023-02-16 21:05:51 +0300

    task 30

---
+++ task 30.py
@@ -0,0 +1,21 @@
+# Задача 30: Заполните массив элементами арифметической прогрессии. 
+# Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
+#  Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+
+def input_num():
+    return int(input("Введите число: "))
+
+def arithmetic_progression(a,b,c):
+    list_arith= list()
+    for i in range(c):
+        list_arith.append(a)
+        a+=b
+    return list_arith
+
+fist_num = input_num()
+dif_num = input_num()
+coin_list = input_num()
+
+print(arithmetic_progression(fist_num, dif_num, coin_list))
